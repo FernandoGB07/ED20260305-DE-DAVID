550f42d7d534b281e0fac21b7169e089bce33aa3
Fernando García Benito <f.garben2007@gmail.com>
2026-03-10 12:38:02 +0100

    Estructura de la Tabla de Multiplicar corregida.
    Correcion a la pasada tabla de multiplicar cuya estructura era la
    incorrecta.

--- src/jcolonia/daw2025/tablasmvc/TablaMultiplicar.java
+++ src/jcolonia/daw2025/tablasmvc/TablaMultiplicar.java
@@ -3,12 +3,35 @@ package jcolonia.daw2025.tablasmvc;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.List;
 
 	/**
-	 * Tabla de multiplicar
+	 * Tabla de multiplicar del programa.
 	 */
 	public class TablaMultiplicar {
+		private int numero;
+		private List<String> listaTextos;
+	
+		public TablaMultiplicar(int número) {
+			
+		}
+		public void generarTabla() {
+			
+		}
+		public List<String> toListaExportación () {
+			return listaTextos;
+			
+		}
 		
+		public List<String> toListaPantalla () {
+			return listaTextos;
+			
+		}
+		
+		public String toString() {
+			return null;
+			
+		}
 	}
 
 
