daebb4dd8d1a96cc73a24a16161a1caebe5c1c45
Fernando García Benito <f.garben2007@gmail.com>
2026-03-10 12:07:02 +0100

    Vista General Incompleta
    Clase VistaGeneral incompleta, con los metodos parcialmente creados,
    pero exentos de contenido en la mayor parte.

---
+++ src/jcolonia/daw2025/tablasmvc/VistaGeneral.java
@@ -0,0 +1,44 @@
+package jcolonia.daw2025.tablasmvc;
+
+import java.util.List;
+
+public class VistaGeneral {
+	String FORMATO_PRINTF_MOSTRARTEXTO;
+	String FORMATO_PRINTF_MOSTRARAVISO;
+
+	public void mostrarTexto(String texto) {
+		
+	}
+	
+	public void mostrarAviso(String texto) {
+		System.out.println("Seleccione el numero acorde a que seccion del menu quiera elegir.")
+	}
+	
+	public void mostrarTítulo(String texto) {
+		
+	}
+	
+	public void mostrarTítulo2(String texto) {
+		
+	}
+	
+	public int pedirnumero(String texto) {
+		return 0;
+	}
+	
+	public void pausa(String texto) {
+		
+	}
+	
+	public boolean pedirConfirmación(String texto) {
+		return false;
+	}
+	public mostrarlista(List<String> lista) {
+		
+		
+	}
+	
+	public getScEntrada() {
+		
+	}
+}
