e0bee14166c98930305031e97c7dfcbc9840fde0
Fernando García Benito <f.garben2007@gmail.com>
2026-03-12 10:15:58 +0100

    Se han iniciado los metodos en VistaMenu
    Se han iniciado los metodos pero incompletos en VistaMenu

--- src/jcolonia/daw2025/tablasmvc/VistaMenú.java
+++ src/jcolonia/daw2025/tablasmvc/VistaMenú.java
@@ -1,5 +1,29 @@
 package jcolonia.daw2025.tablasmvc;
 
+import java.util.List;
+
 public class VistaMenú {
+	private String título;
+	private List<String> opciones;
 	
+	public void Menu(String título, List<String>opciones) {
+
+	}
+
+	public void mostrartítulo() {
+		
+	}
+	
+	public void mostraropciones() {
+		
+	}
+	
+	public int pedirOpción() {
+		return 0;
+		
+	}
+	
+	public void mostrarTexto(String texto) {
+		mostrarTexto(texto);
+	}
 }
