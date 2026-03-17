5d25ec2bbc87e870ef93978e0886f6ca4ff0b500
Fernando García Benito <f.garben2007@gmail.com>
2026-03-17 10:41:45 +0100

    He añadido la herencia y metido todos los metodos de VistaGeneral en VistaMenú

--- src/jcolonia/daw2025/tablasmvc/VistaMenú.java
+++ src/jcolonia/daw2025/tablasmvc/VistaMenú.java
@@ -2,28 +2,35 @@ package jcolonia.daw2025.tablasmvc;
 
 import java.util.List;
 
-public class VistaMenú {
-	private String título;
-	private List<String> opciones;
-	
-	public void Menu(String título, List<String>opciones) {
+public class VistaMenú extends VistaGeneral {
+    private String titulo;
+    private List<String> opciones;
 
-	}
+    public VistaMenú(String título, List<String> opciones) {
+        this.titulo = título;
+        this.opciones = opciones;
+    }
 
-	public void mostrartítulo() {
-		
-	}
-	
-	public void mostraropciones() {
-		
-	}
-	
-	public int pedirOpción() {
-		return 0;
-		
-	}
-	
-	public void mostrarTexto(String texto) {
-		mostrarTexto(texto);
-	}
+    public void mostrArtítulo() {
+        super.mostrarTítulo(titulo);
+    }
+
+    public void mostrarOpciones() {
+        if (opciones != null) {
+            for (String linea : opciones) {
+                System.out.println(linea);
+            }
+        }
+    }
+
+    public int pedirOpción() {
+        mostrArtítulo();
+        mostrarOpciones();
+        return VistaGeneral.pedirNúmero("Seleccione una opcion");
+    }
+
+    @Override
+    public void mostrarTexto(String texto) {
+        super.mostrarTexto(texto);
+    }
 }
