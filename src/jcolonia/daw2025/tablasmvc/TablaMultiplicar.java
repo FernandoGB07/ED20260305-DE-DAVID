800a37c76461dcf7924c00a995af5b9502d0a494
Fernando García Benito <f.garben2007@gmail.com>
2026-03-17 10:28:05 +0100

    Finalizacion Metodos TablaMultiplicar
    Documentacion y metodos de la clase TablaMultiplicar

--- src/jcolonia/daw2025/tablasmvc/TablaMultiplicar.java
+++ src/jcolonia/daw2025/tablasmvc/TablaMultiplicar.java
@@ -3,34 +3,56 @@ package jcolonia.daw2025.tablasmvc;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
 
 	/**
-	 * Tabla de multiplicar del programa.
+	 * La tabla de multiplicar del programa.
 	 */
 	public class TablaMultiplicar {
 		private int numero;
 		private List<String> listaTextos;
 	
-		public TablaMultiplicar(int número) {
-			
+		/**
+		 * El constructor para la Tabla de Multiplicar
+		 * @param numero El numero del cual se hara la tabla de multiplicar.
+		 */
+		public TablaMultiplicar(int numero) {
+			this.numero= numero;
+			this.listaTextos = new ArrayList<>();
 		}
+		/**
+		 * 
+		 */
 		public void generarTabla() {
+			listaTextos.clear();
+	        for (int i = 0; i <= 10; i++) {
+	            listaTextos.add(numero + " x " + i + " = " + (numero * i));
+	        }
 			
 		}
+		/**
+	     * Devuelve la lista de strings para que se guarde en el archivo.
+		 * @return La Lista con los textos
+	     */
 		public List<String> toListaExportación () {
-			return listaTextos;
-			
+			return new ArrayList<>(listaTextos);			
 		}
 		
+		/**
+	     * Devuelve la lista de Strings para que se muestre en consola.
+		 * @return Devuelve la lista de Strings
+	     */
 		public List<String> toListaPantalla () {
 			return listaTextos;
 			
 		}
-		
+		/**
+	     * Enseña la tabla del numero indicado.
+		 * @return La tabla del numero indicado
+	     */
 		public String toString() {
-			return null;
-			
+			return "Tabla del " + numero;
 		}
 	}
 
